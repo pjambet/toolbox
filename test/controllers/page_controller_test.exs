080ef7e61a441bf0bd03b86c4b57c137db02ec35
Pierre Jambet <pierre.jambet@gmail.com>
2016-03-05 15:59:06 -0800

    phoenix new

---
+++ test/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule Toolbox.PageControllerTest do
+  use Toolbox.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
